ed748e541a02eade08cf345a467f110402885831
Mufakkharul Islam Nayem <mufakkharul.nayem@bjitgroup.com>
2019-11-15 10:12:22 +0600

    Initial Commit

---
+++ GradientFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradientFun
+//
+//  Created by Mufakkharul Islam Nayem on 11/15/19.
+//  Copyright © 2019 Mufakkharul Islam Nayem. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
